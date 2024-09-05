858e22a75b3f55c5694a17afebf6b8414baeade3
Abigail Getman-Pickering <abigail.getman.pickering@gmail.com>
2024-09-05 14:26:46 -0400

    update oryx hash

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
 #define USB_SUSPEND_WAKEUP_DELAY 200 // Hopefully this resolves issues with not working after sleep
-#define FIRMWARE_VERSION u8"vrpJO/yR7Dz" // This must match the url of the layout on Oryx for Live Training and Keymapp to work
+#define FIRMWARE_VERSION u8"vrpJO/JWxOO" // This must match the url of the layout on Oryx for Live Training and Keymapp to work
 #define RAW_USAGE_PAGE 0xFF60
 #define RAW_USAGE_ID 0x61
 #define LAYER_STATE_8BIT
